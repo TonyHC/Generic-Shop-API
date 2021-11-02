58be5d87be211dc205c0176d3b86623979c41f79
Tony Chao <tchao@mail.usf.edu>
2021-11-01 23:49:28 -0400

    Implement the GET API for Categories

---
+++ src/main/java/com/springframework/springmvcrest/controller/CategoryController.java
@@ -0,0 +1,38 @@
+package com.springframework.springmvcrest.controller;
+
+import com.springframework.springmvcrest.api.model.CategoryDTO;
+import com.springframework.springmvcrest.api.model.CategoryListDTO;
+import com.springframework.springmvcrest.service.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+// Could also use @RestController:
+// convenience annotation that does nothing more than adding the @Controller and @ResponseBody annotations
+// @ResponseBody activated by default (no need to add the annotation for each request mapping) and indicates that the
+// return type should be written straight to the HTTP response body (and not placed in a Model, or interpreted as a view name)
+@Controller
+@RequestMapping("api/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    // ResponseEntity is meant to represent the entire HTTP response
+    // You can control anything that goes into it: status code, headers, and body
+    // CategoryListDTO object is written to the HTTP Response Entity
+    @GetMapping()
+    public ResponseEntity<CategoryListDTO> getAllCategories() {
+        return new ResponseEntity<>(new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
+        return new ResponseEntity<>(categoryService.getCategoryByName(name), HttpStatus.OK);
+    }
+}
---
+++ src/test/java/com/springframework/springmvcrest/controller/CategoryControllerTest.java
@@ -0,0 +1,84 @@
+package com.springframework.springmvcrest.controller;
+
+import com.springframework.springmvcrest.api.model.CategoryDTO;
+import com.springframework.springmvcrest.service.CategoryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryControllerTest {
+    public static final String NAME = "Thomas";
+    public static final long ID = 1L;
+    @Mock
+    CategoryService categoryService;
+
+    @InjectMocks
+    CategoryController categoryController;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(categoryController).build();
+    }
+
+    @Test
+    void getAllCategoriesTest() throws Exception {
+        // Given
+        CategoryDTO categoryDTO1 = new CategoryDTO();
+        categoryDTO1.setId(ID);
+        categoryDTO1.setName(NAME);
+
+        CategoryDTO categoryDTO2 = new CategoryDTO();
+        categoryDTO2.setId(2L);
+        categoryDTO2.setName("Rob");
+
+        List<CategoryDTO> categoryDTOList = Arrays.asList(categoryDTO1, categoryDTO2);
+
+        // When
+        when(categoryService.getAllCategories()).thenReturn(categoryDTOList);
+
+        // Then
+        mockMvc.perform(get("/api/categories")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                // Look at the JSON Root and go to the categories property
+                .andExpect(jsonPath("$.categories", hasSize(2)));
+    }
+
+    @Test
+    void getCategoryByNameTest() throws Exception {
+        // Given
+        CategoryDTO categoryDTO = new CategoryDTO();
+        categoryDTO.setId(ID);
+        categoryDTO.setName(NAME);
+
+        // When
+        when(categoryService.getCategoryByName(anyString())).thenReturn(categoryDTO);
+
+        // Then
+        mockMvc.perform(get("/api/categories/Jin")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", equalTo(NAME)));
+    }
+}
