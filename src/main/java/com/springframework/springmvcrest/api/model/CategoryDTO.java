47d11ade2905c65bce61eccf67e0db3210b1ad79
Tony Chao <tchao@mail.usf.edu>
2021-11-01 21:36:06 -0400

    Added and Used MapStruct for Category

--- src/main/java/com/springframework/springmvcrest/api/model/CategoryDTO.java
+++ src/main/java/com/springframework/springmvcrest/api/model/CategoryDTO.java
@@ -1,5 +1,8 @@
 package com.springframework.springmvcrest.api.model;
 
+import lombok.Data;
+
+@Data
 public class CategoryDTO {
     private Long id;
     private String name;
---
+++ src/test/java/com/springframework/springmvcrest/api/mapper/CategoryMapperTest.java
@@ -0,0 +1,26 @@
+package com.springframework.springmvcrest.api.mapper;
+
+import com.springframework.springmvcrest.api.model.CategoryDTO;
+import com.springframework.springmvcrest.domain.Category;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryMapperTest {
+    CategoryMapper categoryMapper = CategoryMapper.INSTANCE;
+
+    @Test
+    void categoryToCategoryDTOTest() {
+        // Given
+        Category category = new Category();
+        category.setName("Thomas");
+        category.setId(1L);
+
+        // When
+        CategoryDTO categoryDTO = categoryMapper.categoryToCategoryDTO(category);
+
+        // Then
+        assertEquals(Long.valueOf(1L), categoryDTO.getId());
+        assertEquals("Thomas", categoryDTO.getName());
+    }
+}
