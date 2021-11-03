d772ea42f75faa07b95c39d063a8dd0c420a60d9
Tony Chao <tchao@mail.usf.edu>
2021-11-02 21:07:42 -0400

    Refactor Controllers to use RestController Annotation instead of Controller Annotation

--- src/main/java/com/springframework/springmvcrest/controller/CategoryController.java
+++ src/main/java/com/springframework/springmvcrest/controller/CategoryController.java
@@ -4,17 +4,14 @@ import com.springframework.springmvcrest.api.model.CategoryDTO;
 import com.springframework.springmvcrest.api.model.CategoryListDTO;
 import com.springframework.springmvcrest.service.CategoryService;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-// Could also use @RestController:
-// convenience annotation that does nothing more than adding the @Controller and @ResponseBody annotations
-// @ResponseBody activated by default (no need to add the annotation for each request mapping) and indicates that the
-// return type should be written straight to the HTTP response body (and not placed in a Model, or interpreted as a view name)
-@Controller
+/*
+* @RestController: convenience annotation that does nothing more than adding the @Controller and @ResponseBody annotations
+* @ResponseBody activated by default (no need to add the annotation for each request mapping) and indicates that the
+* return type should be written straight to the HTTP response body (and not placed in a Model, or interpreted as a view name)
+*/
+@RestController
 @RequestMapping(CategoryController.CATEGORY_BASE_URL)
 public class CategoryController {
     public static final String CATEGORY_BASE_URL = "/api/categories";
@@ -25,16 +22,20 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    // ResponseEntity is meant to represent the entire HTTP response
-    // You can control anything that goes into it: status code, headers, and body
-    // CategoryListDTO object is written to the HTTP Response Entity
+    /*
+    * ResponseEntity is meant to represent the entire HTTP response
+    * You can control anything that goes into it: status code, headers, and body
+    * CategoryListDTO object is written to the HTTP Response Entity
+    */
     @GetMapping()
-    public ResponseEntity<CategoryListDTO> getAllCategories() {
-        return new ResponseEntity<>(new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryListDTO getAllCategories() {
+        return new CategoryListDTO(categoryService.getAllCategories());
     }
 
     @GetMapping("/{name}")
-    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
-        return new ResponseEntity<>(categoryService.getCategoryByName(name), HttpStatus.OK);
+    @ResponseStatus(HttpStatus.OK)
+    public CategoryDTO getCategoryByName(@PathVariable String name) {
+        return categoryService.getCategoryByName(name);
     }
 }
