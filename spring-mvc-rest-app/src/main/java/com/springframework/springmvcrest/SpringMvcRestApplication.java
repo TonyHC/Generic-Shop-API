bfc1aedcb68b7deaea0d206342788a8018edcf2f
Tony Chao <tchao@mail.usf.edu>
2021-11-06 14:58:49 -0400

    Refactor App to a Multi-Module Maven Project with Spring Boot

--- spring-mvc-rest-app/src/main/java/com/springframework/springmvcrest/SpringMvcRestApplication.java
+++ spring-mvc-rest-app/src/main/java/com/springframework/springmvcrest/SpringMvcRestApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringMvcRestApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringMvcRestApplication.class, args);
     }
-
 }
